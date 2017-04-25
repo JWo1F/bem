c600f2df8132b92ee33837d14d8fbbe7ef11435b
JWo1F <savegod1@gmail.com>
2017-04-25 18:16:17 +0300

    mixins

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@ type BoolHash = {
 };
 
 interface BemInterface {
-  mod(name:string):BemInterface;
+  mod(name:string): BemInterface;
+  mod(name:BoolHash): BemInterface;
+  mod(name:string[]): BemInterface;
+
+  mix(name:string): BemInterface;
+  mix(name:BoolHash): BemInterface;
+  mix(name:string[]): BemInterface;
 }
 
 interface BemRootInterface extends BemInterface {
@@ -11,24 +17,40 @@ interface BemRootInterface extends BemInterface {
 }
 
 interface BemElementInterface extends BemInterface {
+  parent:string;
 }
 
-abstract class BemAbstract {
+abstract class BemAbstract{
   protected name:string = '';
   protected mods:string[] = [];
   protected mixes:string[] = [];
   protected abstract clone(): BemAbstract;
+  protected abstract stringify(): string;
 
   constructor(name:string) {
     this.name = name;
   }
 
-  mod(name:string): BemAbstract {
+  public mod(name: string|string[]|BoolHash): BemAbstract {
     const cur = this.clone();
+    const arr = (typeof name == 'string') ? name.split(' ') : (name instanceof Array ? name : hashToArr(name));
 
-    cur.mods.push(name);
+    cur.mods.push(...arr);
 
     return cur;
+  }
+
+  public mix(name: string|string[]|BoolHash): BemAbstract {
+    const cur = this.clone();
+    const arr = (typeof name == 'string') ? name.split(' ') : (name instanceof Array ? name : hashToArr(name));
+
+    cur.mixes.push(...arr);
+
+    return cur;
+  }
+
+  public toString(): string {
+    return this.stringify();
   }
 }
 
@@ -42,18 +64,56 @@ export class BemRoot extends BemAbstract implements BemRootInterface {
     return element;
   }
 
-  element(name:string): BemElement {
-    return new BemElement(name);
+  public element(name:string): BemElement {
+    return new BemElement(name, this.name);
+  }
+
+  public stringify(): string {
+    const result:string[] = [];
+
+    result.push(...this.mods.map(mod => this.name + '_' + mod));
+    result.push(...this.mixes);
+
+    return result.join(' ');
   }
 }
 
 class BemElement extends BemAbstract implements BemElementInterface {
+  parent:string;
+
+  constructor(name:string, parent:string) {
+    super(name);
+
+    this.parent = parent;
+  }
+
   protected clone(): BemElement {
-    const element = new BemElement(this.name);
+    const element = new BemElement(this.name, this.parent);
 
     element.mixes = [...this.mixes];
     element.mods = [...this.mods];
 
     return element;
   }
+
+  public stringify(): string {
+    const result:string[] = [];
+
+    result.push(...this.mods.map(mod => this.name + '__' + this.parent + '_' + mod));
+    result.push(...this.mixes);
+
+    return result.join(' ');
+  }
+}
+
+function hashToArr(hash:BoolHash): string[] {
+  const result:string[] = [];
+
+  for(let key in hash) {
+    if(hash[key]) {
+      result.push(key);
+    }
+  }
+
+  return result;
 }
